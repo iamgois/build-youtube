f2c70fd90093c7ca9a03b60c8dc9bce224c6a241
Thiago <thiagogoisjunior@gmail.com>
2024-03-17 12:14:51 -0300

    ep 02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Navbar from '../../src/app/components/Navbar'
+import Main from '../../src/app/components/Main'
 
 export default function Home() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Main />
+    </>
   );
 }
